309d6acdbc8108dcaf0c252bd086b21f548b59fd
Daniel Paul Grech Pereira <13129650+pereiradaniel@users.noreply.github.com>
2020-06-21 13:27:05 -0400

    Fixed memory leak issues.

--- Saiyan.cpp
+++ Saiyan.cpp
@@ -11,47 +11,47 @@ namespace sdds
 	Saiyan::Saiyan()
 	{
 		// default state
-		m_name = nullptr;	// Dynamic allocation:  set to nullptr!
-		m_dob = 0;
-		m_power = 0;
-		m_super = false;
-		m_level = 0;
+		//m_name = nullptr;	// Dynamic allocation:  set to nullptr!
+		//m_dob = 0;
+		//m_power = 0;
+		//m_super = false;
+		//m_level = 0;
 	}
 	
-	Saiyan::Saiyan(const char* name, int dob, int power)
-	{
+	Saiyan::Saiyan(const char* name, int dob, int power) {
 		set(name, dob, power);
 	}
 
-	// MEMBER FUNCTIONS:
 	void Saiyan::set(const char* name, int dob, int power, int level, bool super)
 	{
-		// Check if arguments are valid:
-		if (name == nullptr || strlen(name) <= 0 || dob > 2020 || power <= 0)
+		if (name != nullptr && name[0] != '\0')
 		{
-			*this = Saiyan();	// Calls constructor that creates default.
-		}
-		else
-		{
-			// Deallocate previosly allocated memory for m_name to avoid memory leak:
-			if (m_name != nullptr && strlen(name) == 0)
+			if (m_name != nullptr)
 			{
 				delete[] m_name;
 				m_name = nullptr;
 			}
-			// Assign validate values to current object:
 			m_name = new char[strlen(name) + 1];
 			strcpy(m_name, name);
+		}
+		if (dob != 0 && dob < 2020)
+		{
 			m_dob = dob;
+		}
+		if (power > 0)
+		{
 			m_power = power;
-			m_super = super;
+		}
+		if (level > 0)
+		{
 			m_level = level;
 		}
+		m_super = super;
 	}
 	
 	bool Saiyan::isValid() const
 	{
-		bool valid_state = m_name != nullptr && strlen(m_name) > 0 && m_dob < 2020 && m_power > 0;
+		bool valid_state = m_name != nullptr && m_dob != 0 && m_dob < 2020 && m_power > 0 && m_level >= 0;
 		return valid_state;
 	}
 
--- Saiyan.h
+++ Saiyan.h
@@ -6,11 +6,11 @@ namespace sdds
 {
 	class Saiyan
 	{
-		char* m_name;		// Dynamically allocated array of chars.
-		int m_dob;			// Year the Saiyan was born.
-		int m_power;		// Integer indicating the strength of the Saiyan (>= 0).
-		bool m_super;		// indicates whether Saiyan can evolve
-		int m_level;		// an integer indicating the level of a SS
+		char* m_name{};		// Dynamically allocated array of chars.
+		int m_dob{};				// Year the Saiyan was born.
+		int m_power{};			// Integer indicating the strength of the Saiyan (>= 0).
+		bool m_super{};		// indicates whether Saiyan can evolve
+		int m_level{};			// an integer indicating the level of a SS
 
 		/*
 		***Valid Name*** : a dynamically allocated array of chars.
